9e460b30ee9ce7d6b717f62f7e6766ff2044a267
Hell Vision <2856782165@qq.com>
2021-08-28 18:11:45 +0800

    【初始化】初始化Markdown

--- src/tool/markdown/markdown.js
+++ src/tool/markdown/markdown.js
@@ -1,6 +1,7 @@
 import MarkdownItVue from 'markdown-it-vue'
 import 'markdown-it-vue/dist/markdown-it-vue.css'
 
+// noinspection JSUnusedLocalSymbols
 const markdown = {
     install: function(Vue, option) {
         Vue.component('markdown', MarkdownItVue)
