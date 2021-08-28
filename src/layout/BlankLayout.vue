1973018cc14790d28f5f328c0850a29194d43e88
Hell Vision <2856782165@qq.com>
2021-08-28 23:27:58 +0800

    【功能新增】路由机制与空布局

---
+++ src/layout/BlankLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="border: #42b983 2px">
+        <router-view />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'BlankLayout',
+    mounted() {
+        console.log('BlankLayout=========', this.$router)
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,70 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import BlankLayout from "@/layout/BlankLayout"
 
 Vue.use(Router)
+
+const RouteView = {
+    name: 'RouteView',
+    render: h => h('router-view')
+}
 
 let router = new Router({
     routes: [
         {
             path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
+            redirect: '/temp',
+            component: BlankLayout,
+            meta: {title: 'home'},
+            children: [
+                {
+                    path: '/temp',
+                    redirect: '/home',
+                    component: RouteView,
+                    meta: {title: 'menu.dashboard'},
+                    children: [
+                        {
+                            path: '/home',
+                            name: 'HomePage',
+                            component: () => import('@/components/HelloWorld'),
+                            meta: {title: 'home.title'}
+                        },
+                    ]
+                },
+            ]
+        },
+        // 异常页面
+        {
+            path: '/exception',
+            name: 'exception',
+            component: RouteView,
+            redirect: '/exception/403',
+            meta: {title: 'menu.exception', icon: 'warning', permission: ['exception']},
+            children: [
+                {
+                    path: '/exception/403',
+                    name: 'Exception403',
+                    component: () => import(/* webpackChunkName: "fail" */ '@/page/exception/403'),
+                    meta: {title: 'menu.exception.not-permission', permission: ['exception']}
+                },
+                {
+                    path: '/exception/404',
+                    name: 'Exception404',
+                    component: () => import(/* webpackChunkName: "fail" */ '@/page/exception/404'),
+                    meta: {title: 'menu.exception.not-find', permission: ['exception']}
+                },
+                {
+                    path: '/exception/500',
+                    name: 'Exception500',
+                    component: () => import(/* webpackChunkName: "fail" */ '@/page/exception/500'),
+                    meta: {title: 'menu.exception.server-error', permission: ['exception']}
+                }
+            ]
+        },
+        // 404重定向
+        {
+            path: '*',
+            redirect: '/exception/404',
         }
     ]
 })
